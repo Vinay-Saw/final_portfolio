f973c017b02f998f9dd4b4e6194d758fc2853c2b
Vinay Kumar Saw <112120404+Vinay-Saw@users.noreply.github.com>
2025-09-20 09:58:15 +0530

    Switch from HashRouter to BrowserRouter

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-// Import HashRouter instead of BrowserRouter
-import { HashRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import Projects from "./pages/Projects";
 import ProjectDetail from "./pages/ProjectDetail";
@@ -19,8 +18,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      {/* Use HashRouter here */}
-      <HashRouter>
+      <BrowserRouter basename="/vinay-saw.github.io-8f1c2becf3e7649fd7b6afcefd67504fb869aabb/">
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/projects" element={<Projects />} />
@@ -29,7 +27,7 @@ const App = () => (
           <Route path="/certificates/:id" element={<CertificateDetail />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
-      </HashRouter>
+      </BrowserRouter>
     </TooltipProvider>
   </QueryClientProvider>
 );
